44719a8ee2254f70e60b0e9395620b81c3df9707
ev <ev@evang.dk>
2014-09-01 09:32:15 +0200

    add project to git

---
+++ TankVsTank/Game1.cs
@@ -0,0 +1,104 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using TankVsTank.GameFrameWork;
+
+#endregion
+
+namespace TankVsTank
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : GameHost
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Tank _tank;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _tankTexture;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+                _spriteBatch = new SpriteBatch(GraphicsDevice); 
+                _tankTexture = Content.Load<Texture2D>("car.png"); 
+                _tank = new Tank(this, new Vector2(100,100), _tankTexture);
+                GameObjects.Add(_tank);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            _spriteBatch.Begin();
+            foreach (SpriteObject gameObject in GameObjects)
+            {
+                gameObject.Draw(gameTime, _spriteBatch);
+            }
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TankVsTank/Tank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TankVsTank.GameFrameWork;
+
+namespace TankVsTank
+{
+    class Tank : SpriteObject
+    {
+        public Tank(GameHost game)
+            : base(game)
+        {
+            // Set the default scale and color
+            ScaleX = 1;
+            ScaleY = 1;
+            SpriteColor = Color.White;
+        }
+
+        public Tank(GameHost game, Vector2 position)
+            : this(game)
+        {
+            // Store the provided position
+            Position = position;
+        }
+
+
+       public Tank(GameHost game, Vector2 position, Texture2D texture)
+            : this(game, position)
+        {
+            // Store the provided texture
+            SpriteTexture = texture;
+        }
+       
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        
+    }
+}
